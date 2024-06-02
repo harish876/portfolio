11873388ef69b48f882a606966ef8c64783a70b7
harish876 <harishgokul01@gmail.com>
2024-06-02 13:30:13 +0530

    new ui changes

--- handlers/commandFactory.go
+++ handlers/commandFactory.go
@@ -7,18 +7,18 @@ import (
 	"github.com/harish876/portfolio/models"
 	"github.com/harish876/portfolio/views/about"
 	"github.com/harish876/portfolio/views/help"
+	"github.com/harish876/portfolio/views/home"
 	"github.com/harish876/portfolio/views/project"
 	"github.com/labstack/echo/v4"
 )
 
 var (
-	ABOUT    = "about"
-	AUTHOR   = "author"
+	README   = "readme"
 	PROJECT  = "project"
 	PROJECTS = "projects"
 	BLOG     = "blog"
 	BLOGS    = "blogs"
-	HOME     = "home"
+	HOME     = "cd ~/"
 
 	HELP = "help"
 )
@@ -26,13 +26,12 @@ var (
 // TODO: move this to a different logical group
 var (
 	HELP_OPTIONS = []models.Commands{
-		{Command: "about", Description: "check all available commands"},
 		{Command: "readme", Description: "check out the guy who made this (using htmx btw)"},
-		{Command: "man", Description: "check all keys only navigation options"},
+		{Command: "man", Description: "check all keys only navigation options (coming soon)"},
 		{Command: "project", Description: "check out all my projects"},
-		{Command: "blog", Description: "check out all my blogs"},
+		{Command: "blog", Description: "check out all my blogs (coming soon)"},
 		{Command: "cd ~/", Description: "go back to the home page"},
-		{Command: "profile", Description: "go to my socials"},
+		{Command: "profile", Description: "go to my socials (coming soon)"},
 	}
 )
 
@@ -40,7 +39,9 @@ func CommandHandler(c echo.Context) error {
 	command := c.FormValue("command")
 	command = strings.ToLower(command)
 	switch command {
-	case ABOUT:
+	case HOME:
+		return Render(c, http.StatusOK, home.Home())
+	case README:
 		return Render(c, http.StatusOK, about.About())
 	case PROJECT, PROJECTS:
 		data, _ := GetProjectsDataFromGithubHandler()
@@ -48,7 +49,6 @@ func CommandHandler(c echo.Context) error {
 	case HELP:
 		return Render(c, http.StatusOK, help.Help(HELP_OPTIONS))
 	default:
-		//todo make a new one
-		return Render(c, http.StatusOK, about.About())
+		return Render(c, http.StatusOK, home.Home())
 	}
 }
