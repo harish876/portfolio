c97715d6ec0f3bf9362e024f3e59d9ebb0e5d571
harish876 <harishgokul01@gmail.com>
2024-06-02 09:34:57 +0530

    vercel tings

--- api/index.go
+++ api/index.go
@@ -3,8 +3,34 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/harish876/portfolio/handlers"
+	"github.com/harish876/portfolio/routes"
+	"github.com/harish876/portfolio/utils"
+
+	"github.com/labstack/echo/v4"
 )
 
+func Init() *echo.Echo {
+	if _, err := utils.GetEnv(); err != nil {
+		fmt.Println("Unable to get env file")
+		//log.Fatal(err)
+	}
+
+	app := echo.New()
+	app.Static("/", "assets")
+
+	app.HTTPErrorHandler = handlers.NotFoundErrorHandler
+	routes.RegisterHomeRoutes(app)
+	routes.RegisterAboutRoutes(app)
+	routes.RegisterCommandRoutes(app)
+	routes.RegisterProjectRoutes(app)
+	routes.RegisterProjectApiRoutes(app)
+
+	return app
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	app := Init()
+	app.Logger.Fatal(app.Start(":42069"))
 }
