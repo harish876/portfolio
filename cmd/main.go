688ed1999b3030e0b7cc29ca2a1e4ec896a7f3bf
harish876 <harishgokul01@gmail.com>
2024-06-02 12:17:05 +0530

    removed env check temp

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"log"
-
 	"github.com/harish876/portfolio/handlers"
 	"github.com/harish876/portfolio/routes"
-	"github.com/harish876/portfolio/utils"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	if _, err := utils.GetEnv(); err != nil {
-		log.Fatal(err)
-	}
+	// if _, err := utils.GetEnv(); err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	app := echo.New()
 	app.Static("/", "assets")
